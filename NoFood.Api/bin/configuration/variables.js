b4b040d943df08057c3d5c1a4288e7bbb8d8bf62
TiagoBruckmann <tiago_bruck@outlook.com.br>
2020-12-07 23:27:38 -0300

    upd api

--- NoFood.Api/bin/configuration/variables.js
+++ NoFood.Api/bin/configuration/variables.js
@@ -3,7 +3,7 @@ const variables = {
         port: process.env.port || 3000
     },
     Database: {
-        connection: process.env.connection || 'mongodb://admin:nofood123456@ds257981.mlab.com:57981/nofood'
+        connection: process.env.connection || 'mongodb+srv://admin:1234@cluster0.fwa0p.mongodb.net/nofood?retryWrites=true&w=majority'
     },
     Security: {
         secretyKey: 'd41d8cd98f00b204e9800998ecf8427e|7aef61337bcee2fe773aa78b40afacbc'
